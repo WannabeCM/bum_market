f223723351a38e1d05f05facd9ff7ff0fbe4f879
Jason Park <stiger3820@gmail.com>
2022-06-19 22:38:30 +0900

    add

--- bum-market-react/my-app/src/main/index.jsx
+++ bum-market-react/my-app/src/main/index.jsx
@@ -1,16 +1,24 @@
+import { useState, useEffect } from "react";
 import "./CSS/index.css";
 import "./CSS/reset.css";
 import axios from "axios";
 
 function MainPage() {
-  axios
-    .get("https://bef77cc5-9ada-4bed-a622-00909f2581e0.mock.pstmn.io/products")
-    .then((result) => {
-      console.log("실행 결과: ", result);
-    })
-    .catch((error) => {
-      console.log("에러 발생: ", error);
-    });
+  const [products, setProducts] = useState([]);
+  useEffect(function () {
+    axios
+      .get(
+        "https://bef77cc5-9ada-4bed-a622-00909f2581e0.mock.pstmn.io/products"
+      )
+      .then((result) => {
+        console.log("실행 결과: ", result);
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch((error) => {
+        console.log("에러 발생: ", error);
+      });
+  }, []);
 
   return (
     <>
@@ -25,27 +33,27 @@ function MainPage() {
         </article>
         <h1>대기중인 파티모임</h1>
         <article id="product-list">
-          <div className="product-card">
-            <div>
-              <img
-                class="product-img"
-                src="images/products/nftmarket.png"
-                alt=""
-              />
-            </div>
-            <div className="product-contents">
-              <span class="product-name">박성범</span>
-              <span class="product-price">3000</span>
-              <div class="product-seller">
-                <img
-                  class="product-avatar"
-                  src="images/icons/avatar.png"
-                  alt=""
-                />
-                <span>범이</span>
+          {products.map(function (products, index) {
+            return (
+              <div className="product-card">
+                <div>
+                  <img class="product-img" src={products.imageUrl} alt="" />
+                </div>
+                <div className="product-contents">
+                  <span class="product-name">{products.name}</span>
+                  <span class="product-price">{products.price}원</span>
+                  <div class="product-seller">
+                    <img
+                      class="product-avatar"
+                      src="images/icons/avatar.png"
+                      alt=""
+                    />
+                    <span>{products.seller}</span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </article>
       </main>
       <footer id="footer"></footer>
