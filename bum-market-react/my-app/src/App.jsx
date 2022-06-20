24b985f62fcb554f5fef13e9e9fec7a47230ec20
Jason Park <stiger3820@gmail.com>
2022-06-20 10:35:59 +0900

    add:상품상세페이지추가

--- bum-market-react/my-app/src/App.jsx
+++ bum-market-react/my-app/src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
-import Main from "./main";
+import MainPage from "./main";
+import { Routes, Route, Link } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product/index.jsx";
 
 function App() {
   return (
     <>
-      <Main />
+      <nav>
+        <Link to={"/"}>MainPage</Link>
+        <Link to={"/product"}>Product</Link>
+        <Link to={"/upload"}>Upload</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/products/:id" element={<ProductPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+      </Routes>
     </>
   );
 }
